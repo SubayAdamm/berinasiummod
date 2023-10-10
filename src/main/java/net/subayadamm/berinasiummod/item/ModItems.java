07ddc6d33ffdc047b58ac96afc2bfc1caf88fe93
SubayAdamm <subayadamm@gmail.com>
2023-10-10 15:26:54 +0200

    added sword registries etc

--- src/main/java/net/subayadamm/berinasiummod/item/ModItems.java
+++ src/main/java/net/subayadamm/berinasiummod/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.subayadamm.berinasiummod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +11,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BerinasiumMod.MOD_ID);
 
-    //mines etc
+    //ores
     public static final RegistryObject<Item> BERINASIUM = ITEMS.register("berinasium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
     public static final RegistryObject<Item> RAW_BERINASIUM = ITEMS.register("raw_berinasium",
@@ -22,7 +21,29 @@ public class ModItems {
     public static final RegistryObject<Item> BERINASIUM_CHUNK = ITEMS.register("berinasium_chunk",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
-    //blocks
+    //equipment
+    public static final RegistryObject<Item> BERINASIUM_LONGSWORD = ITEMS.register("berinasium_longsword",
+            () -> new SwordItem(Tiers.NETHERITE, 10, 5f,
+                    new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> BERINASIUM_WARAXE = ITEMS.register("berinasium_waraxe",
+            () -> new AxeItem(Tiers.NETHERITE, 10, 5f,
+                    new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> DIAMOND_WARAXE = ITEMS.register("diamond_waraxe",
+            () -> new AxeItem(Tiers.NETHERITE, 10, 5f,
+                    new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> BERINASIUM_KNIFE = ITEMS.register("berinasium_knife",
+            () -> new SwordItem(Tiers.NETHERITE, 10, 5f,
+                    new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> STEEL_KNIFE = ITEMS.register("steel_knife",
+            () -> new SwordItem(Tiers.IRON, 10, 5f,
+                    new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+
+
+    //dismantled stuff
+    public static final RegistryObject<Item> DIAMOND_AXE_HEAD = ITEMS.register("diamond_axe_head",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
 
 
     public static void register(IEventBus eventBus) {
