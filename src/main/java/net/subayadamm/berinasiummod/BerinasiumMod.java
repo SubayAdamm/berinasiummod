400b4f9d7f6fd03ce1308657e786f1ba3e1f6899
SubayAdamm <subayadamm@gmail.com>
2023-12-05 09:58:25 +0100

    added some recipes

--- src/main/java/net/subayadamm/berinasiummod/BerinasiumMod.java
+++ src/main/java/net/subayadamm/berinasiummod/BerinasiumMod.java
@@ -2,31 +2,24 @@ package net.subayadamm.berinasiummod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.IEventExceptionHandler;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.subayadamm.berinasiummod.block.ModBlocks;
 import net.subayadamm.berinasiummod.item.ModItems;
 import net.subayadamm.berinasiummod.world.feature.ModConfiguredFeatures;
 import net.subayadamm.berinasiummod.world.feature.ModPlacedFeatures;
 import org.slf4j.Logger;
+
 
 @Mod(BerinasiumMod.MOD_ID)
 public class BerinasiumMod
